03a2e36292e08d2d674a60d0d1ee2562e38528e4
torkolenko <torkolenkoa@gmail.com>
2024-06-14 14:52:41 +0300

    fix styles host

--- services/host/src/bootstrap.tsx
+++ services/host/src/bootstrap.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/Router";
+import "./reset.scss";
 
 const root = document.getElementById("root");
 
